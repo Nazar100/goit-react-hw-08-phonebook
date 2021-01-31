356fb304d01ee9723b15e02281e8b0beccf2ca50
Nazar Leshchyshyn <lesh1.nazar@gmail.com>
2021-01-31 11:41:26 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { store, persistor } from './redux/store';
 
 ReactDOM.render(
@@ -24,4 +23,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
